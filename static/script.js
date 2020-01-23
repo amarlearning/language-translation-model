435a84af2b399a5ddcb6ef62e555e175f26cb0e4
Amar Prakash Pandey <amar.om1994@gmail.com>
2020-01-24 00:33:51 +0530

    Added request loading bar

--- static/script.js
+++ static/script.js
@@ -6,23 +6,24 @@ $("#form").submit(function(event) {
 
     event.preventDefault();
 
+    $("#pageloader").fadeIn();
+
     var $form = $(this),
-    text = $form.find('input[name="french-text"]').val(),
-    url = $form.attr('action');
+    url = $form.attr('action'),
+    text = $form.find('input[name="french-text"]').val();
 
     var posting = $.post(url, {
         frenchtext: text
     });
 
     posting.done(function(data) {
-       
         var $output = data
-
         if ($output.length == 0) {
             $('#show_hide').hide();
         } else {
             $('#show_hide').show();
             $('.card-b').text($output)
+            $("#pageloader").fadeOut();
         }
     });
 });
